9f37238c0a0c1bd30bdb8e1ff21238fa944f43c8
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-12 08:36:23 -0700

    fixing formatting

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 		<title>Snap-polyglot</title>
 	</head>
 	<body>
-
 		<h1>
 			&#191;Quien fue unos de los poetas &#225;ms importantes en la historia de Mexico?
 		<br>Sor Juana In&#233;s de la Cruz.  Uno de sus eleg&#237;as m&#225;s importantes fue &#187;De la belad de Laura enamorados
