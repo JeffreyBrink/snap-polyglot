5ca61dcd6a44f71cecf9b87bfae8d9aeb51ceb1d
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-12 08:23:38 -0700

    added spanish symbols

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Snap-polyglot</title>
+	</head>
+	<body>
+		<p>&#191;Quien fue unos de los poetas &#225;ms importantes en la historia de Mexico?
+		<br>Sor Juana In&#233;s de la Cruz.  Uno de sus eleg&#237;as m&#225;s importantes fue &#187;De la belad de Laura enamorados
+		</p>
+		<br>
+		<p>Who was one of the most important poets in the histpry of Mexico?
+		<br>Sor Juana Ines de la Cruz.  One of her most important selections was De la beldad de Laura enamorados.</p>
+	</body>
+</html>
